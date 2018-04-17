ea536d3cf67ef0ed525740d039114337539b7f8b
筠筑 <bamboo.cutecat@gmail.com>
2018-04-17 15:32:48 +0800

    Ver2.5.1
    model修正

--- python_code/cnn_keras_model.py
+++ python_code/cnn_keras_model.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 # from keras.backend.tensorflow_backend import set_session
 import pandas as pd
 import numpy as np
@@ -41,26 +42,28 @@ from keras.preprocessing.image import img_to_array
 from numpy import float
 from multi_gpu_model_fixed import multi_gpu_model
 
+stocknum = {
+    '0051', '1102', '1216', '1227', '1314', '1319', '1434', '1451', '1476',
+    '1477', '1504', '1536', '1560', '1590', '1605', '1704', '1717', '1718',
+    '1722', '1723', '1789', '1802', '1909', '2015', '2049', '2059', '2106',
+    '2201', '2204', '2207', '2227', '2231', '2312', '2313', '2324', '2327',
+    '2337', '2344', '2347', '2352', '2353', '2356', '2360', '2371', '2376',
+    '2377', '2379', '2385', '2439', '2448', '2449', '2451', '2478', '2492',
+    '2498', '2542', '2603', '2606', '2610', '2615', '2618', '2723', '2809',
+    '2812', '2834', '2845', '2867', '2888', '2912', '2915', '3019', '3034',
+    '3044', '3051', '3189', '3231', '3406', '3443', '3532', '3673', '3682',
+    '3702', '3706', '4137', '4915', '4943', '4958', '5264', '5522', '5871',
+    '6005', '6116', '6176', '6239', '6269', '6285', '6409', '6414', '6415',
+    '6452', '6456', '8454', '8464', '9910', '9914', '9917', '9921', '9933',
+    '9938', '9941', '9945'
+}
 
-stocknum = {'0051', '1102', '1216', '1227', '1314', '1319', '1434', '1451', '1476', '1477', '1504', '1536', '1560', '1590',
-            '1605', '1704', '1717', '1718', '1722', '1723', '1789', '1802', '1909', '2015', '2049', '2059', '2106', '2201',
-            '2204', '2207', '2227', '2231', '2312', '2313', '2324', '2327', '2337', '2344', '2347', '2352', '2353', '2356',
-            '2360', '2371', '2376', '2377', '2379', '2385', '2439', '2448', '2449', '2451', '2478', '2492', '2498', '2542',
-            '2603', '2606', '2610', '2615', '2618', '2723', '2809', '2812', '2834', '2845', '2867', '2888', '2912', '2915',
-            '3019', '3034', '3044', '3051', '3189', '3231', '3406', '3443', '3532', '3673', '3682', '3702', '3706', '4137',
-            '4915', '4943', '4958', '5264', '5522', '5871', '6005', '6116', '6176', '6239', '6269', '6285', '6409', '6414',
-            '6415', '6452', '6456', '8454', '8464', '9910', '9914', '9917', '9921', '9933', '9938', '9941', '9945'}
 
-
-#hdf5_path = "pic_sum_new.h5"
-
-
-def generate_train_from_file(x, y, batch_size):
+def generate_from_file(x, y, batch_size):
 
     ylen = len(y)
-    #loopcount = ylen // batch_size
 
-    while(True):
+    while (True):
         #X_output = np.zeros((batch_size,) + (224, 224, 3), dtype=np.float)
         Data = []
 
@@ -75,39 +78,11 @@ def generate_train_from_file(x, y, batch_size):
             #X_output[i] = pic
             Data.append(pic)
 
-        Data = np.array(Data, dtype=np.uint8).reshape(
-            (batch_size,) + (224, 224, 3))
+        Data = np.array(
+            Data, dtype=np.uint8).reshape((batch_size, ) + (224, 224, 3))
         # print(Data.shape)
 
         yield Data, labels
-
-
-def generate_val_from_file(x):
-
-    xlen = len(x)
-    #loopcount = ylen // batch_size
-
-    #while(True):
-
-    #X_output = np.zeros((batch_size,) + (224, 224, 3), dtype=np.float)
-    Data = []
-
-    #i = randint(0, loopcount)
-    #labels = np.array(y)
-
-    for i, pic_addr in enumerate(x):
-            pic = imageio.imread(pic_addr)
-            #X_output[i] = pic
-            Data.append(pic)
-            if i % 100 == 0:
-                print(str(i)+' / '+str(xlen)+'   data done')
-
-    Data = np.array(Data, dtype=np.uint8).reshape((xlen,) + (224, 224, 3))
-    # print(Data.shape)
-
-    return Data
-
-    #    yield Data, labels
 
 
 def main():
@@ -120,16 +95,17 @@ def main():
     table_sum = []
 
     for stockid in stocknum:
-        df = pd.read_hdf('table/'+stockid +
-                         '_table_sumchange.h5', 'stock_data_table')
+        df = pd.read_hdf('table/' + stockid + '_table_sumchange.h5',
+                         'stock_data_table')
         #pic_addr = glob.glob('stock_pic/' +stockid + 'pic/'+'*.jpg')
         pic_addrs = []
         for addr in range(len(df)):
-            if (os.path.exists('stock_pic/' + stockid + 'pic/'
-                               + str(addr).zfill(4)+'_'+stockid+'.jpg')) == False:
-                break
+            if (os.path.exists('stock_pic/' + stockid + 'pic/' + str(addr)
+                               .zfill(4) + '_' + stockid + '.jpg')) == False:
+                print('FileNotFoundError error\n')
+                raise FileNotFoundError
             pic_addrs.append('stock_pic/' + stockid + 'pic/' +
-                             str(addr).zfill(4)+'_'+stockid+'.jpg')
+                             str(addr).zfill(4) + '_' + stockid + '.jpg')
         #pic_addr = pic_addr.sort()
         # print(pic_addrs)
         for item in pic_addrs:
@@ -149,84 +125,105 @@ def main():
     addrs = list(addrs)
     labels = list(labels)
 
-    # train_addrs = addrs[0:int(0.7*len(addrs))]
-    # train_labels = labels[0:int(0.7*len(labels))]
-    # val_addrs = addrs[int(0.7*len(addrs)):int(0.9*len(addrs))]
-    # val_labels = labels[int(0.7*len(addrs)):int(0.9*len(addrs))]
-    # test_addrs = addrs[int(0.9*len(addrs)):]
-    # test_labels = labels[int(0.9*len(labels)):]
+    train_addrs = addrs[0:int(0.7 * len(addrs))]
+    train_labels = labels[0:int(0.7 * len(labels))]
+    val_addrs = addrs[int(0.7 * len(addrs)):int(0.9 * len(addrs))]
+    val_labels = labels[int(0.7 * len(addrs)):int(0.9 * len(addrs))]
+    test_addrs = addrs[int(0.9 * len(addrs)):]
+    test_labels = labels[int(0.9 * len(labels)):]
 
     with tf.device('/cpu:0'):
-        model = ResNet50(input_shape=(224, 224, 3), classes=3,
-                         pooling='max', include_top=True, weights=None)
+        model = ResNet50(
+            input_shape=(224, 224, 3),
+            classes=3,
+            pooling='max',
+            include_top=True,
+            weights=None)
 
     parallel_model = multi_gpu_model(model, gpus=4)
-    parallel_model.compile(loss='categorical_crossentropy',
-                           optimizer='adam', metrics=['accuracy'])
+    parallel_model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy'])
 
     parallel_model.summary()
     print('\n\n\n\n')
 
-    # filepath="best_acc.h5"
-    #checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-    # early_stop = EarlyStopping(monitor='acc', min_delta=0.0001, patience=5, verbose=1, mode='max')
+    # if not os.path.exists('sum_pic.h5'):
+    #     sum_pic = h5py.File('sum_pic.h5', mode='w')
 
-    # callbacks_list = [checkpoint, early_stop]
-    # myAdam = Adam(decay=0.1)
+    #     sum_pic.create_dataset(
+    #         'pic', shape=(len(addrs), 224, 224, 3), dtype=np.uint8)
 
-    #model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    # train_history = model.fit(x=x_train, y=y_train, epochs=3, batch_size=100, shuffle=True, callbacks = callbacks_list)
-    if not os.path.exists('sum_pic.h5'):
-        sum_pic = h5py.File('sum_pic.h5', mode='w')
+    #     for i, addr in enumerate(addrs):
+    #         pic = imageio.imread(addr)
+    #         sum_pic['pic'][i] = pic
+    #         if i % 100 == 0:
+    #             print(str(i) + ' / ' + str(len(addrs)) + '  pic done')
 
-        sum_pic.create_dataset('pic', shape=(
-            len(addrs), 224, 224, 3), dtype=np.uint8)
+    #     sum_pic.create_dataset('table', data=labels, dtype=np.uint8)
 
-        for i, addr in enumerate(addrs):
-            pic = imageio.imread(addr)
-            sum_pic['pic'][i] = pic
-            if i % 100 == 0:
-                print(str(i)+' / '+str(len(addrs))+'  pic done')
+    # else:
+    #     sum_pic = h5py.File('sum_pic.h5', mode='r')
 
-        sum_pic.create_dataset('table', data=labels, dtype=np.uint8)
+    batch_size = 10
 
-    else:
-        sum_pic = h5py.File('sum_pic.h5', mode='r')
+    model_checkpoint = ModelCheckpoint(
+        "best_acc.h5",
+        monitor='val_acc',
+        verbose=1,
+        save_best_only=True,
+        mode='max')
 
-    batch_size = 100
-    #train_generator = train_datagen.flow_from_directory(  '')
-    # print multiprocessing.cpu_count()
-    #output_directory = 'model_checkpoint/'
+    # train_history = parallel_model.fit(x=sum_pic['pic'][0:int(len(sum_pic['table'])*0.9)],
+    #                                    y=sum_pic['table'][0:int(
+    #                                        len(sum_pic['table'])*0.9)],
+    #                                    #steps_per_epoch=None,
+    #                                    steps_per_epoch=50000,
+    #                                    epochs=1,
+    #                                    #batch_size=batch_size,
+    #                                    verbose=1, callbacks=[model_checkpoint],
+    #                                    validation_split=0.2, shuffle=True)
 
-    model_checkpoint = ModelCheckpoint("best_acc.h5", monitor='val_acc',
-                                       verbose=1, save_best_only=True, mode='max')
+    train_history = parallel_model.fit_generator(
+        generate_from_file(train_addrs, train_labels, batch_size),
+        steps_per_epoch=len(train_addrs) // batch_size,
+        #steps_per_epoch=100,
+        epochs=10,
+        verbose=1,
+        validation_data=generate_from_file(val_addrs, val_labels, batch_size),
+        validation_steps=len(val_addrs) // batch_size,
+        #validation_steps=100,
+        workers=mp.cpu_count(),
+        max_queue_size=1000,
+        shuffle=True,
+        callbacks=[model_checkpoint],
+        use_multiprocessing=True)
 
-    train_history = parallel_model.fit(x=sum_pic['pic'][0:int(len(sum_pic['table'])*0.9)],
-                                       y=sum_pic['table'][0:int(
-                                           len(sum_pic['table'])*0.9)],
-                                       steps_per_epoch=None,
-                                       #steps_per_epoch=50000,
-                                       epochs=10,
-                                       batch_size=batch_size,
-                                       verbose=1, callbacks=[model_checkpoint],
-                                       validation_split=0.2, shuffle=True)
-
-    loss, accuracy = parallel_model.evaluate(
-        x=sum_pic['pic'][int(len(sum_pic['table'])*0.9)
-                             :int(len(sum_pic['table'])*1.0)],
-        y=sum_pic['table'][int(len(sum_pic['table'])*0.9)
-                               :int(len(sum_pic['table'])*1.0)],
-        verbose=1, batch_size=batch_size)
+    loss, accuracy = parallel_model.evaluate_generator(
+        generate_from_file(test_addrs, test_labels, batch_size),
+        # val_samples=len(test_addrs),
+        steps=len(test_addrs) // batch_size,
+        workers=mp.cpu_count(),
+        use_multiprocessing=True,
+        verbose=1,
+        max_queue_size=1000)
 
     print(train_history)
     plt.figure()
     plt.plot(train_history.history['acc'])
-    plt.savefig('model_accuracy'+str(round(accuracy, 2)) +
-                '.pdf', dpi=200, bbox_inches='tight', mode='w')
+    plt.savefig(
+        'model_accuracy' + str(round(accuracy, 2)) + '.pdf',
+        dpi=200,
+        bbox_inches='tight',
+        mode='w')
     plt.figure()
     plt.plot(train_history.history['loss'])
-    plt.savefig('model_loss'+str(round(loss, 2))+'.pdf',
-                dpi=200, bbox_inches='tight', mode='w')
+    plt.savefig(
+        'model_loss' + str(round(loss, 2)) + '.pdf',
+        dpi=200,
+        bbox_inches='tight',
+        mode='w')
 
     print('\ntest loss: ', loss)
     print('\ntest accuracy: ', accuracy)
@@ -242,7 +239,7 @@ def main():
     # print(list(parallel_model.predict(data,batch_size=10,verbose=1)))
 
     #accPrint = int(accuracy * 10000)
-    parallel_model.save('my_model_'+str(round(accuracy, 2))+'.h5')
+    parallel_model.save('my_model_' + str(round(accuracy, 2)) + '.h5')
 
     # ------------ save the template model rather than the gpu_mode ----------------
     # serialize model to JSON
